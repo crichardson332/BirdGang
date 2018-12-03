b18f91637c364d2c5120900aa31f8c60e8fc34b6
Christopher Richardson <christopher.richardson@gtri.gatech.edu>
2018-12-03 18:43:54 -0500

    add going out tab

---
+++ BirdGang/GoingOutViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GoingOutViewController.swift
+//  BirdGang
+//
+//  Created by Chris Richardson on 12/3/18.
+//  Copyright © 2018 Chris Richardson. All rights reserved.
+//
+
+import UIKit
+
+class GoingOutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
