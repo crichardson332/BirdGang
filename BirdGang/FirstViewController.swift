490f701ddefb3d97567dc35f34c61618143942e3
Christopher Richardson <christopher.richardson@gtri.gatech.edu>
2018-12-02 20:29:31 -0500

    Initial Commit

---
+++ BirdGang/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  BirdGang
+//
+//  Created by Chris Richardson on 12/2/18.
+//  Copyright © 2018 Chris Richardson. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ BirdGang/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  BirdGang
+//
+//  Created by Chris Richardson on 12/2/18.
+//  Copyright © 2018 Chris Richardson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
