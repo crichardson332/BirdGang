0ecbf007eff3d8898fefdfe45e5e05dca0b0fe7a
Christopher Richardson <christopher.richardson@gtri.gatech.edu>
2018-12-03 19:03:38 -0500

    restoring files that disappeared without explanation

--- BirdGang/AppDelegate.swift
+++ BirdGang/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyBdVl-cTICSwYKrZ95SuvNw7dbMuDt1KG0")
+        // GMSPlacesClient.provideAPIKey("AIzaSyBdVl-cTICSwYKrZ95SuvNw7dbMuDt1KG0")
         return true
     }
 
@@ -43,4 +46,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
-
--- BirdGang/ORIGINALGoingOutViewController.swift
+++ BirdGang/ORIGINALGoingOutViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class GoingOutViewController: UIViewController {
 
@@ -15,7 +16,22 @@ class GoingOutViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
+
+    override func loadView() {
+        // Create a GMSCameraPosition that tells the map to display the
+        // coordinate -33.86,151.20 at zoom level 6.
+        let camera = GMSCameraPosition.camera(withLatitude: 33.785, longitude: -84.385, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: 33.785, longitude: -84.385)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+    }
+
 
     /*
     // MARK: - Navigation
