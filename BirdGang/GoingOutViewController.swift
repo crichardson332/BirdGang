880b467fc00a1a8cc65e7d7410bedc2adb5d1d7a
Christopher Richardson <christopher.richardson@gtri.gatech.edu>
2018-12-03 19:32:59 -0500

    squash

--- BirdGang/GoingOutViewController.swift
+++ BirdGang/GoingOutViewController.swift
@@ -20,15 +20,17 @@ class GoingOutViewController: UIViewController {
     override func loadView() {
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: 33.785, longitude: -84.385, zoom: 6.0)
+        let camera = GMSCameraPosition.camera(withLatitude: 33.785, longitude: -84.385, zoom: 15.0)
+        // let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         view = mapView
 
         // Creates a marker in the center of the map.
         let marker = GMSMarker()
         marker.position = CLLocationCoordinate2D(latitude: 33.785, longitude: -84.385)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
+        // marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Publico"
+        marker.snippet = "Atlanta"
         marker.map = mapView
     }
 
