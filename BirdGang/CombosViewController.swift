b22ba3d20b179591453bfb7cec94cc3cbb15bca0
Christopher Richardson <christopher.richardson@gtri.gatech.edu>
2018-12-03 20:35:43 -0500

    add slider for bar chart

--- BirdGang/CombosViewController.swift
+++ BirdGang/CombosViewController.swift
@@ -9,13 +9,25 @@
 import UIKit
 import Charts
 
+
 class CombosViewController: UIViewController {
+    
+    @IBOutlet weak var number1: UISlider!
+    @IBOutlet weak var barChart: BarChartView!
+    
+    @IBAction func renderCharts() {
+        barChartUpdate()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        barChartUpdate()
     }
-
-
+    
+    func barChartUpdate () {
+        //future home of bar chart code
+    }
+    
 }
 
