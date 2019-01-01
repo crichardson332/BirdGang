6458d9218fc197df37990b2ac118119e94fac45f
Christopher Richardson <christopher.richardson@gtri.gatech.edu>
2019-01-01 18:42:16 -0500

    add buttons for going-out vc navigation

--- BirdGang/GoingOutViewController.swift
+++ BirdGang/GoingOutViewController.swift
@@ -7,31 +7,10 @@
 //
 
 import UIKit
-import MapKit
 
 class GoingOutViewController: UIViewController {
 
-    @IBOutlet weak var mapView: MKMapView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // set initial location to Publico
-        let initialLocation = CLLocation(latitude: 33.784824, longitude: -84.384605)
-        let labelLocation = CLLocationCoordinate2D(latitude: 33.784824, longitude: -84.384605)
-        centerMapOnLocation(location: initialLocation)
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = labelLocation
-        annotation.title = "Publico"
-        annotation.subtitle = "Atlanta"
-        mapView.addAnnotation(annotation)
     }
-
-    // map size
-    let regionRadius: CLLocationDistance = 1000
-    
-    func centerMapOnLocation(location: CLLocation) {
-        let coordinateRegion = MKCoordinateRegion(center: location.coordinate,latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
-        mapView.setRegion(coordinateRegion, animated: true)
-    }
-
 }
---
+++ BirdGang/MapViewController.swift
@@ -0,0 +1,37 @@
+//
+//  GoingOutViewController.swift
+//  BirdGang
+//
+//  Created by Chris Richardson on 12/3/18.
+//  Copyright © 2018 Chris Richardson. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // set initial location to Publico
+        let initialLocation = CLLocation(latitude: 33.784824, longitude: -84.384605)
+        let labelLocation = CLLocationCoordinate2D(latitude: 33.784824, longitude: -84.384605)
+        centerMapOnLocation(location: initialLocation)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = labelLocation
+        annotation.title = "Publico"
+        annotation.subtitle = "Atlanta"
+        mapView.addAnnotation(annotation)
+    }
+
+    // map size
+    let regionRadius: CLLocationDistance = 1000
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegion(center: location.coordinate,latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+
+}
